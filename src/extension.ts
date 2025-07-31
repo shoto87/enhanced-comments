70161419f80d2a3fb4a841df7566b22ef5d34580
pratik-ssww <pratik.ssww@gmail.com>
2025-07-31 18:08:23 +0530

    added files , solved bug

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  // Grammar is registered via package.json, no need for code here
-}
+export function activate(context: vscode.ExtensionContext) {}
+export function deactivate() {}
